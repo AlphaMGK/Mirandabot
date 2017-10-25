89590880e88c2625adbc0d7ccceb4b5433d8a979
burhanahmeed <burhanahmeed@gmail.com>
2017-10-25 18:44:55 +0700

    kesekian

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,37 @@
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const reco = new SpeechRecognition();
 const socket = io();
 
-reco.lang = 'en-US';
-reco.interimResult = false;
 
 const outputYou = document.querySelector('.output-you');
 const outputBot = document.querySelector('.output-bot');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const reco = new SpeechRecognition();
+
+reco.lang = 'en-US';
+reco.interimResult = false;
+reco.maxAlternatives = 1;
+
 document.querySelector('button').addEventListener('click', function(){
 	reco.start();
+});
+reco.addEventListener('speechstart', () => {
+  console.log('Speech has been detected.');
 });
 
 reco.addEventListener('result', function(e){
 	let last = e.result.length - 1;
-	let txt = e.result[last][0].transcrip;
+	let text = e.result[last][0].transcrip;
 	outputYou.textContent = text;
 
 	console.log('confidence: '+e.result[0][0].confidence);
-	socket.emit('chat msg', txt);
+	socket.emit('chat msg', text);
+});
+reco.addEventListener('speechend', () => {
+  recognition.stop();
+});
+
+reco.addEventListener('error', (e) => {
+  outputBot.textContent = 'Error: ' + e.error;
 });
 
 function synthVoice(text) {
