1689904e9d5f0a278c0fbe178a152865e4d757f3
burhanahmeed <burhanahmeed@gmail.com>
2017-10-25 18:09:59 +0700

    environmet set

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const app = express();
 	app.use(express.static(__dirname + '/views'));
 	app.use(express.static(__dirname + '/public'));
 
-	const server = app.listen(process.env.PORT || 5000, console.log('berhasil, listen in port 5000'));
+	const server = app.listen(process.env.PORT || 5000, function(){
+		console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+	});
 	
 	const apiai = require('apiai')(APIAI_TOKEN);
 	const io = require('socket.io')(server);
