950c488b40bc870354bcd8386398fc0bee90b5c2
burhanahmeed <burhanahmeed@gmail.com>
2017-10-25 16:55:55 +0700

    miranda bot

---
+++ index.js
@@ -0,0 +1,44 @@
+// const APIAI_TOKEN = process.env.APIAI_TOKEN;
+// const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
+const APIAI_TOKEN = '183d7ca8b0e04a56beb8baee896d9a2d';
+const APIAI_SESSION_ID = '12345';
+
+const express = require('express');
+const app = express();
+
+	app.use(express.static(__dirname + '/views'));
+	app.use(express.static(__dirname + '/public'));
+
+	const server = app.listen(5000, console.log('berhasil, listen in port 5000'));
+	
+	const apiai = require('apiai')(APIAI_TOKEN);
+	const io = require('socket.io')(server);
+
+	io.on('connection', function(socket){
+		console.log('user connected');
+	});
+
+	app.get('/', function(req, res) {
+		res.sendFile('index.html');
+	});
+
+	io.on('connection', function(socket){
+		socket.on('chat msg', function(text){
+			console.log(text);
+			
+			let apiaiReq = apiai.textRequest(text, {
+			sessionId: APIAI_SESSION_ID
+			});
+
+			apiaiReq.on('response', function(response) {
+			let aiText = response.result.fulfillment.speech;
+			socket.emit('bot reply', aiText); // Send the result back to the browser!
+			});
+
+			apiaiReq.on('error', function(error){
+			console.log(error);
+			});
+
+			apiaiReq.end();
+		});
+	});
---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+const speechRecognition = window.speechRecognition ||  window.webkitSpeechRecognition;
+const reco = new speechRecognition();
+const socket = io();
+
+reco.lang = 'en-US';
+reco.interimResult = false;
+
+const outputYou = document.querySelector('.output-you');
+const outputBot = document.querySelector('.output-bot');
+document.querySelector('button').addEventListener('click', function(){
+	reco.start();
+});
+
+reco.addEventListener('result', function(e){
+	let last = e.result.length - 1;
+	let txt = e.result[last][0].transcrip;
+	outputYou.textContent = text;
+
+	console.log('confidence: '+e.result[0][0].confidence);
+	socket.emit('chat msg', txt);
+});
+
+function synthVoice(text) {
+	const synth = window.speechSynthesis;
+	const utterance = new SpeechSynthesisUtterance();
+	utterance.text = text;
+	synth.speak(utterance);
+}
+
+socket.on('bot reply', function(replyText) {
+	synthVoice(replyText);
+
+	if(replyText == '') replyText = '(No answer...)';
+	outputBot.textContent = replyText;
+});
